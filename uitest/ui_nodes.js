3952b9a4e8c7ab7c9de4761b622b6b445fdd234d
Patrick Soquet <ps@moddable.tech>
2022-09-24 17:05:34 +0200

    backup

--- uitest/ui_nodes.js
+++ uitest/ui_nodes.js
@@ -2,7 +2,11 @@ import { Node } from "nodered";
 import {
 	REDButton,
 	REDDropDown,
+	REDGauge,
+	REDNumeric,
+	REDSlider,
 	REDSpacer,
+	REDSwitch,
 	REDTextRowLeft,
 	REDTextRowCenter,
 	REDTextRowRight,
@@ -71,6 +75,11 @@ function position(group, control) {
 		lines[top].fill(1, x, right);
 }
 
+function registerConstructor(type, constructor) {
+	constructor.type = type;
+	RED.nodes.registerType(type, constructor);
+}
+
 class UINode extends Node {
 	constructor(id, flow, name) {
 		super(id, flow, name);
@@ -82,16 +91,33 @@ class UINode extends Node {
 	}
 }
 
+class UIBaseNode extends Node {
+	constructor(id, flow, name) {
+		super(id, flow, name);
+	}
+	onStart(config) {
+		super.onStart(config);
+	}
+}
+registerConstructor("ui_base", UIBaseNode);
+
 class UIControlNode extends UINode {
 	constructor(id, flow, name) {
 		super(id, flow, name);
 	}
 	onStart(config) {
 		super.onStart(config);
-		this.width = parseInt(config.width);
-		this.height = parseInt(config.height);
 		const groupNode = RED.nodes.getNode(config.group);
 		insert(groupNode.controls, this);
+		this.width = parseInt(config.width);
+		this.height = parseInt(config.height);
+		if (this.width == 0)
+			this.width = groupNode.width;
+		if (this.height == 0)
+			this.height = this.defaultHeight;
+	}
+	get defaultHeight() {
+		return 1;
 	}
 }
 
@@ -112,26 +138,157 @@ class UIButtonNode extends UIControlNode {
 		this.send(this.msg);
 	}
 }
-RED.nodes.registerType("ui_button", UIButtonNode);
+registerConstructor("ui_button", UIButtonNode);
 
 class UIDropDownNode extends UIControlNode {
 	constructor(id, flow, name) {
 		super(id, flow, name);
 	}
+	onChanged(tracking) {
+		this.msg.payload = this.options[this.selection].value;
+		this.send(this.msg);
+	}
+	onMessage(msg) {
+		this.selection = this.options.findIndex(option => option.value == msg.payload);
+		this.container.delegate("onUpdate");
+		if (this.passthru && (this.msg._msgid != msg._msgid))
+			this.send(msg);
+	}
 	onStart(config) {
 		super.onStart(config);
 		this.label = config.label;
-		this.options = config.options.map(option => { 
-			return { label:option.label, msg: { payload:option.value, topic:config.topic } }; 
-		});
-		this.place = config.place;
+		this.options = config.options;
+		this.passthru = config.passthru;
+		this.placeHolder = config.place;
+		this.selection = -1;
+		
 		this.Template = REDDropDown;
+		
+		this.msg = { topic: config.topic }
 	}
 	onSelect(index) {
 		this.send(this.options[index].msg);
 	}
 }
-RED.nodes.registerType("ui_dropdown", UIDropDownNode);
+registerConstructor("ui_dropdown", UIDropDownNode);
+
+class UIGaugeNode extends UIControlNode {
+	constructor(id, flow, name) {
+		super(id, flow, name);
+	}
+	onMessage(msg) {
+		const { min, max } = this;
+		let value = Number(msg.payload);
+		if (value < min)
+			value = min;
+		else if (value > max)
+			value = max;
+		this.value = value;
+		this.container.delegate("onUpdate");
+	}
+	onStart(config) {
+		super.onStart(config);
+		this.colors = config.colors;
+		const min = this.min = config.min;
+		const max = this.max = config.max;
+		let seg1 = config.seg1;
+		let seg2 = config.seg2;
+		if ((seg1 != "") && (seg2 != "")) {
+			seg1 =  Math.min(max, Math.max(min, Number(seg1)));
+			seg2 =  Math.min(max, Math.max(min, Number(seg2)));
+			this.seg1 = Math.min(seg1, seg2);
+			this.seg2 = Math.max(seg1, seg2);
+		}
+		else {
+			this.seg1 = undefined;
+			this.seg2 = undefined;
+		}
+		this.title = config.title;
+		this.value = this.min;
+		this.Template = REDGauge;
+	}
+	get defaultHeight() {
+		return Math.round(this.width * 2 / 3);
+	}
+}
+registerConstructor("ui_gauge", UIGaugeNode);
+
+class UINumericNode extends UIControlNode {
+	constructor(id, flow, name) {
+		super(id, flow, name);
+	}
+	onChanged(tracking) {
+		this.msg.payload = this.value;
+		this.send(this.msg);
+	}
+	onMessage(msg) {
+		const { min, max, step } = this;
+		let value = parseInt(msg.payload);
+		if (value < min)
+			value = min;
+		else if (value > max)
+			value = max;
+		else
+			value = Math.round(value / step) * step;
+		this.value = value;
+		this.container.delegate("onUpdate");
+		if (this.passthru && (this.msg._msgid != msg._msgid))
+			this.send(msg);
+	}
+	onStart(config) {
+		super.onStart(config);
+		this.label = config.label;
+		this.min = config.min;
+		this.max = config.max;
+		this.step = config.step;
+		this.passthru = config.passthru;
+		this.value = this.min;
+		this.wrap = config.wrap;
+		
+		this.Template = REDNumeric;
+		
+		this.msg = { topic: config.topic }
+	}
+}
+registerConstructor("ui_numeric", UINumericNode);
+
+class UISliderNode extends UIControlNode {
+	constructor(id, flow, name) {
+		super(id, flow, name);
+	}
+	onChanged(tracking) {
+		this.msg.payload = this.value;
+		this.send(this.msg);
+	}
+	onMessage(msg) {
+		const { min, max, step } = this;
+		let value = parseInt(msg.payload);
+		if (value < min)
+			value = min;
+		else if (value > max)
+			value = max;
+		else
+			value = Math.round(value / step) * step;
+		this.value = value;
+		this.container.delegate("onUpdate");
+		if (this.passthru && (this.msg._msgid != msg._msgid))
+			this.send(msg);
+	}
+	onStart(config) {
+		super.onStart(config);
+		this.label = config.label;
+		this.min = config.min;
+		this.max = config.max;
+		this.step = config.step;
+		this.passthru = config.passthru;
+		this.value = this.min;
+		
+		this.Template = REDSlider;
+		
+		this.msg = { topic: config.topic }
+	}
+}
+registerConstructor("ui_slider", UISliderNode);
 
 class UISpacerNode extends UIControlNode {
 	constructor(id, flow, name) {
@@ -142,18 +299,54 @@ class UISpacerNode extends UIControlNode {
 		this.Template = REDSpacer;
 	}
 }
-RED.nodes.registerType("ui_spacer", UISpacerNode);
+registerConstructor("ui_spacer", UISpacerNode);
+
+class UISwitchNode extends UIControlNode {
+	constructor(id, flow, name) {
+		super(id, flow, name);
+	}
+	onChanged(tracking) {
+		this.msg.payload = this.options[this.selection].value;
+		this.send(this.msg);
+	}
+	onMessage(msg) {
+		this.selection = this.options.findIndex(option => option.value == msg.payload);
+		if (this.selection < 0)
+			this.selection = 0;
+		this.container.delegate("onUpdate");
+		if (this.passthru && (this.msg._msgid != msg._msgid))
+			this.send(msg);
+	}
+	onStart(config) {
+		super.onStart(config);
+		this.label = config.label;
+		this.options = [
+			{ value:config.offvalue, type:config.offvalueType },
+			{ value:config.onvalue, type:config.onvalueType },
+		];
+		this.passthru = config.passthru;
+		this.selection = 0;
+		
+		this.Template = REDSwitch;
+		
+		this.msg = { topic: config.topic }
+	}
+}
+registerConstructor("ui_switch", UISwitchNode);
 
 class UITextNode extends UIControlNode {
 	constructor(id, flow, name) {
 		super(id, flow, name);
 	}
 	onMessage(msg) {
-		this.container.delegate("onMessage", msg.payload);
+		this.value = msg.payload;
+		this.container.delegate("onUpdate");
 	}
 	onStart(config) {
 		super.onStart(config);
 		this.label = config.label;
+		this.value = "";
+		
 		switch (config.layout) {
 		case "row-left": this.Template = REDTextRowLeft; break;
 		case "row-center": this.Template = REDTextRowCenter; break;
@@ -161,10 +354,9 @@ class UITextNode extends UIControlNode {
 		case "col-center": this.Template = REDTextColumnCenter; break;
 		default: this.Template = REDTextRowSpread; break;
 		}
-		
 	}
 }
-RED.nodes.registerType("ui_text", UITextNode);
+registerConstructor("ui_text", UITextNode);
 
 class UIGroupNode extends UINode {
 	constructor(id, flow, name) {
@@ -180,7 +372,7 @@ class UIGroupNode extends UINode {
 		insert(tabNode.groups, this);
 	}
 }
-RED.nodes.registerType("ui_group", UIGroupNode);
+registerConstructor("ui_group", UIGroupNode);
 
 class UITabNode extends UINode {
 	constructor(id, flow, name) {
@@ -192,7 +384,9 @@ class UITabNode extends UINode {
 		insert(model.tabs, this);
 	}
 }
-RED.nodes.registerType("ui_tab", UITabNode);
+registerConstructor("ui_tab", UITabNode);
+
+export { UIControlNode, registerConstructor } 
 
 export default function() {
 	model.tabs.forEach(tab => {
@@ -201,10 +395,6 @@ export default function() {
 			if (group.disp)
 				group.lines.push(new Uint8Array(group.width).fill(1));
 			group.controls.forEach(control => {
-				if (control.width == 0)
-					control.width = group.width;
-				if (control.height == 0)
-					control.height = 1;
 				position(group, control);	
 			});
 			group.height = group.lines.length;
--- uitest/ui_templates.js
+++ uitest/ui_templates.js
@@ -20,6 +20,8 @@ const textures = {
 	button: { path:"button.png" },
 	glyphs: { path:"glyphs.png" },
 	popup: { path:"popup.png" },
+	slider: { path:"slider.png" },
+	switch: { path:"switch.png" },
 };
 const theme = Object.freeze({
 	skins: {
@@ -32,26 +34,40 @@ const theme = Object.freeze({
 		tabMenuItem: { fill:[TRANSPARENT,TRANSPARENT,TRANSPARENT,COLOR] },
 		tabMenuItemIcon: { texture:textures.glyphs, color:[GRAY_4,OFF_WHITE,OFF_WHITE,WHITE], x:200, y:0, width:40, height:40 },
 		tabTitle: { fill:[TRANSPARENT,GRAY_2,GRAY_2,COLOR], stroke:BLACK, bottom:1 },
+		
 		group: { fill:GRAY_2, stroke:GRAY_4, bottom:1 },
 		groupIcon: { texture:textures.glyphs, color:[OFF_WHITE,OFF_WHITE,OFF_WHITE,WHITE], x:40, y:0, width:40, height:40, variants:40 },
 		groupTitle: { fill:GRAY_3, stroke:GRAY_4, bottom:1 },
+		
 		button: { texture:textures.button, color: [TRANSPARENT,GRAY_4,GRAY_4,COLOR], x:0, y:0, width:60, height:60, left:20, right:20, top:20, bottom:20 },
 		
 		dropDown: { texture:textures.popup, color: [TRANSPARENT,GRAY_4,GRAY_4,COLOR], x:0, y:0, width:60, height:60, left:20, right:20, top:20, bottom:20 },
 		dropDownIcon: { texture:textures.glyphs, color:[GRAY_4,OFF_WHITE,OFF_WHITE,WHITE], x:160, y:0, width:40, height:40 },
+
+		numericLeft: { texture:textures.popup, color: [TRANSPARENT,GRAY_4,GRAY_4,COLOR], x:0, y:0, width:40, height:60, top:20, bottom:20 },
+		numericCenter: { texture:textures.popup, color: [TRANSPARENT,GRAY_4,GRAY_4,COLOR], x:20, y:0, width:20, height:60, left:0, right:0, top:20, bottom:20 },
+		numericRight: { texture:textures.popup, color: [TRANSPARENT,GRAY_4,GRAY_4,COLOR], x:20, y:0, width:40, height:60, top:20, bottom:20 },
+		numericLess: { texture:textures.glyphs, color: [GRAY_4,OFF_WHITE,OFF_WHITE,WHITE], x:240, y:0, width:40, height:40 },
+		numericMore: { texture:textures.glyphs, color: [GRAY_4,OFF_WHITE,OFF_WHITE,WHITE], x:280, y:0, width:40, height:40 },
+
+		sliderBar: { texture:textures.slider, color: [GRAY_4,COLOR], x:0, y:0, width:80, height:40, left:20, right:20 },
+		sliderThumb: { texture:textures.slider, color: [OFF_WHITE,WHITE], x:80, y:0, width:40, height:40 },
+		
+		switchBar: { texture:textures.switch, color: [TRANSPARENT,GRAY_4,COLOR], x:0, y:0, width:60, height:40 },
+		switchThumb: { texture:textures.switch, color: [OFF_WHITE,WHITE], x:60, y:0, width:40, height:40 },
 	},
 	styles: {
 		tabTitle: { font:"600 18px Open Sans", color:[OFF_WHITE,OFF_WHITE,OFF_WHITE,WHITE], horizontal:"left", left:10 },
-		tabMenuItem: { font:"600 18px Open Sans", color:[GRAY_4,OFF_WHITE,OFF_WHITE,WHITE], horizontal:"left", left:10 },
+		tabMenuItem: { font:"600 18px Open Sans", color:[GRAY_4,OFF_WHITE,OFF_WHITE,WHITE], horizontal:"left", right:10 },
 		groupTitle: { font:"600 18px Open Sans", color:[OFF_WHITE,OFF_WHITE,OFF_WHITE,WHITE], horizontal:"left", left:10 },
 		button: { font:"600 18px Open Sans", color:[GRAY_4,OFF_WHITE,OFF_WHITE,WHITE] },
-		dropDownLabel: { font:"18px Open Sans", color:OFF_WHITE, left:10, right:10 },
-		textName: { font:"18px Open Sans", color:[OFF_WHITE] },
-		textValue: { font:"600 18px Open Sans", color:[OFF_WHITE] },
-		textNameLeft: { font:"18px Open Sans", color:[OFF_WHITE], horizontal:"left", left:10 },
-		textValueLeft: { font:"600 18px Open Sans", color:[OFF_WHITE], horizontal:"left", left:10 },
-		textNameRight: { font:"18px Open Sans", color:[OFF_WHITE], horizontal:"right", right:10 },
-		textValueRight: { font:"600 18px Open Sans", color:[OFF_WHITE], horizontal:"right", right:10 },
+		
+		textName: { font:"18px Open Sans", color:OFF_WHITE },
+		textValue: { font:"600 18px Open Sans", color:OFF_WHITE },
+		textNameLeft: { font:"18px Open Sans", color:OFF_WHITE, horizontal:"left", left:10 },
+		textValueLeft: { font:"600 18px Open Sans", color:OFF_WHITE, horizontal:"left", left:10 },
+		textNameRight: { font:"18px Open Sans", color:OFF_WHITE, horizontal:"right", right:10 },
+		textValueRight: { font:"600 18px Open Sans", color:OFF_WHITE, horizontal:"right", right:10 },
 	},
 }, true);
 
@@ -133,19 +149,33 @@ let REDButton = Container.template($ => ({
 }));
 
 class REDDropDownBehavior extends ButtonBehavior {
+	onCreate(container, data) {
+		super.onCreate(container, data);
+		data.container = container;
+		let style = globalThis.menuItemStyle;
+		if (!style)
+			style = globalThis.menuItemStyle = new Style(theme.styles.tabMenuItem);
+		let size = style.measure(data.placeHolder);
+		let width = size.width;
+		data.options.forEach(option => {
+			size = style.measure(option.label);
+			if (width < size.width)
+				width = size.width;
+		});
+		width = (Math.floor(width / UNIT) + 1) * UNIT;
+		width += UNIT;
+		container.coordinates = {left:0, width, top:0, bottom:0};
+	}
 	onDisplaying(container) {
 		super.onDisplaying(container);
-		this.selection = -1;
-		container.first.next.string = this.data.place;
+		this.onUpdate(container);
 	}
 	onMenuSelected(container, index) {
 		if ((index >= 0) && (this.selection != index)) {
 			let data = this.data;
-			let item = data.options[index];
-			this.selection = index;
-			data.value = item.value;
-			container.first.next.string = item.label;
-			this.data.onSelect(index);
+			data.selection = index;
+			data.onChanged();
+			this.onUpdate(container);
 		}
 	}
 	onTap(container) {
@@ -153,21 +183,26 @@ class REDDropDownBehavior extends ButtonBehavior {
 		let it = {
 			button: container,
 			items: data.options,
-			selection: this.selection,
+			selection: data.selection,
 		};
 		application.add(new REDDropDownMenu(it));
+	}
+	onUpdate(container) {
+		const data = this.data;
+		const selection = data.selection;
+		container.first.next.string = (selection < 0) ? data.placeHolder : data.options[selection].label;
 	}
 }
 
 let REDDropDown = Row.template($ => ({
 	left:$.left, width:$.width, top:$.top, height:$.height, clip:true,
 	contents: [
-		$.label ? Label($, { top:0, bottom:0, style:theme.styles.dropDownLabel, string:$.label }) : null,
+		Label($, { left:0, right:0, top:0, bottom:0, style:theme.styles.textNameLeft, string:$.label }),
 		Row($, {
-			left:0, right:0, top:0, bottom:0, skin:theme.skins.dropDown, active:true, Behavior: REDDropDownBehavior,
+			left:0, width:0, top:0, bottom:0, skin:theme.skins.dropDown, active:true, Behavior: REDDropDownBehavior,
 			contents: [
 				Content($, { width:UNIT, top:0, bottom:0, skin:theme.skins.dropDownIcon  }),
-				Label($, { left:0, right:0, top:0, bottom:0, style:theme.styles.tabTitle }),
+				Label($, { left:0, right:0, top:0, bottom:0, style:theme.styles.tabMenuItem }),
 			],
 		}),
 	],
@@ -181,31 +216,41 @@ let REDDropDownMenu = Layout.template($ => ({
 			let button = data.button;
 			let container = layout.first;
 			let scroller = container.first;
-			let size = scroller.first.measure();
-			let selection = scroller.first.measure();
-			let y;
-			if (data.selection >= 0)
-				y = Math.max(button.y - ((size.height / data.items.length) * data.selection), 0);
-			else
-				y = button.y + button.height;
-			let height = Math.min(size.height, application.height - y);
-			container.coordinates = { left:button.x - 10, width:button.width + 20, top:y, height:height + 10 };
-			scroller.coordinates = { left:10, width:button.width + 10, top:0, height:height };
+			let layoutHeight = application.height;
+			let menuHeight = data.items.length * UNIT;
+			let y, height;
+			if (layoutHeight > menuHeight) {
+				y = (data.selection >= 0) ? button.y - (data.selection * UNIT) : button.y + UNIT;
+				if (y < 0)
+					y = 0;
+				else if (y > layoutHeight - menuHeight)
+					y = layoutHeight - menuHeight;
+				height = menuHeight;
+			}
+			else {
+				y = 0;
+				height = layoutHeight;
+			}
+			container.coordinates = { left:button.x, width:button.width, top:y, height:height };
+			scroller.coordinates = { left:0, width:button.width, top:0, height:height };
 			if (data.selection >= 0)
 				scroller.first.content(data.selection).first.visible = true;
 			return value;
 		}
 	},
 	contents: [
-		Container($, { contents:[
-			Scroller($, { skin:theme.skins.tabMenu, clip:true, active:true, backgroundTouch:true, Behavior:VerticalScrollerBehavior, 
-				contents:[
-					Column($, { left:0, right:0, top:0, 
-						contents: $.items.map($$ => new REDDropDownMenuItem($$)),
-					}),
-				]
-			}),
-		]}),
+		Container($, {
+			clip:true, skin:theme.skins.dropDown, state:1,
+			contents:[
+				Scroller($, { active:true, backgroundTouch:true, Behavior:VerticalScrollerBehavior, 
+					contents:[
+						Column($, { left:0, right:0, top:0, 
+							contents: $.items.map($$ => new REDDropDownMenuItem($$)),
+						}),
+					]
+				}),
+			]
+		}),
 	],
 }));
 
@@ -218,16 +263,286 @@ let REDDropDownMenuItem = Row.template($ => ({
 	],
 }));
 
+class REDGaugeBehavior extends Behavior {
+	onCreate(container, data) {
+		this.data = data;
+		data.container = container;
+		
+		const shape = container.first;
+		shape.skin = new Skin({fill:GRAY_4, stroke:data.colors});
+	}
+	onDisplaying(container) {
+		this.onUpdate(container);
+	}
+	onUpdate(container) {
+		const data = this.data;
+		const { min, max, value, seg1, seg2 } = data;
+		const fraction = (value - min) / (max - min);
+		const { width, height } = container;
+		const shape = container.first;
+		const label = container.last;
+		
+		const x = width >> 1;
+		const y = height - 10;
+		let r = height - 50;
+		if (r > x) r = x;
+		
+		if (shape.fillOutline == null) {
+			const path = new Outline.CanvasPath;
+			path.arc(x, y, r - 20, Math.PI, 0);
+			shape.fillOutline = Outline.stroke(path, 40, Outline.LINECAP_BUTT, Outline.LINEJOIN_MITER);
+		}
+		const path = new Outline.CanvasPath;
+		path.arc(x, y, r - 20, Math.PI, Math.PI * (1 +  fraction) );
+		shape.strokeOutline = Outline.stroke(path, 40, Outline.LINECAP_BUTT, Outline.LINEJOIN_MITER);
+		if ((seg1 !== undefined) && (seg2 !== undefined))
+			shape.state = (value <= seg1) ? 0 : (value <= seg2) ? 1 : 2;
+		else
+			shape.state = fraction * 2;
+		label.string = value;
+	}
+};
+
+let REDGauge = Container.template($ => ({
+	left:$.left, width:$.width, top:$.top, height:$.height, clip:true, Behavior: REDGaugeBehavior,
+	contents: [
+		Shape(1, { left:0, right:0, top:0, bottom:0 } ),
+		Label($, { left:0, right:0, top:0, height:40, style:theme.styles.textName, string:$.title }),
+		Label($, { left:0, right:0, bottom:0, height:40, style:theme.styles.textValue }),
+	],
+}));
+
+class REDNumericBehavior extends Behavior {
+	onCreate(container, data) {
+		this.data = data;
+		data.container = container;
+	}
+	onDisplaying(container) {
+		this.onUpdate(container);
+	}
+	onTrack(container, direction) {
+		const data = this.data;
+		const { min, max, step } = data;
+		let value = data.value + (direction * step);
+		if (value < min)
+			value = min;
+		else if (value > max)
+			value = max;
+		if (data.value != value) {
+			data.value = value;
+			data.onChanged();
+		}
+		this.onUpdate(container);
+	}
+	onUpdate(container) {
+		container.first.next.string = this.data.value;
+	}
+};
+
+class REDNumericButtonBehavior extends ButtonBehavior {
+	onTap(container) {
+		container.bubble("onTrack", this.data);
+	}
+};
+
+let REDNumeric = Row.template($ => ({
+	left:$.left, width:$.width, top:$.top, height:$.height, clip:true,
+	contents: [
+		Label($, { left:0, right:0, top:0, bottom:0, style:theme.styles.textNameLeft, string:$.label }),
+		Row($, {
+			width:3*UNIT, height:UNIT, Behavior: REDNumericBehavior,
+			contents: [
+				Container(-1, {
+					width:UNIT, height:UNIT, skin:theme.skins.numericLeft, active:true, Behavior: REDNumericButtonBehavior,
+					contents: [
+						Content($, { width:UNIT, height:UNIT, skin:theme.skins.numericLess }),
+					],
+				}),
+				Label($, { width:UNIT, height:UNIT, skin:theme.skins.numericCenter, style:theme.styles.textValue, state:1 }),
+				Container(1, {
+					width:UNIT, height:UNIT, skin:theme.skins.numericRight, active:true, Behavior: REDNumericButtonBehavior,
+					contents: [
+						Content($, { width:UNIT, height:UNIT, skin:theme.skins.numericMore }),
+					],
+				}),
+			],
+		}),
+	],
+}));
+
+class REDSliderBehavior extends Behavior {
+	onCreate(container, data) {
+		this.data = data;
+		data.container = container;
+	}
+	onDisplaying(container) {
+		this.onUpdate(container);
+	}
+	onTouchBegan(container, id, x, y, ticks) {
+		container.captureTouch(id, x, y, ticks);
+		this.onTouchMoved(container, id, x, y, ticks);
+	}
+	onTouchMoved(container, id, x, y, ticks) {
+		const data = this.data;
+		const min = data.min;
+		const max = data.max;
+		const step = data.step;
+		const width = container.last.width;
+		let fraction = (x - (container.x + (width >> 1))) / (container.width - width);
+		let value = min + (fraction * (max - min));
+		if (value < min)
+			value = min;
+		else if (value > max)
+			value = max;
+		else
+			value = Math.round(value / step) * step;
+		data.value = value;
+		data.onChanged(true);
+		this.onUpdate(container);
+	}
+	onTouchEnded(container, id, x, y, ticks) {
+		this.data.onChanged(false);
+	}
+	onUpdate(container) {
+		var button = container.last;
+		var bar = button.previous;
+		const data = this.data;
+		const min = data.min;
+		const max = data.max;
+		const value = data.value;
+		const x = container.x;
+		const width = container.width - button.width;
+		const offset = Math.round(((value - min) / (max - min)) * width);
+		bar.width = offset + button.width;
+		button.x = x + offset;
+	}
+};
+
+let REDSlider = Row.template($ => ({
+	left:$.left, width:$.width, top:$.top, height:$.height, clip:true,
+	contents: [
+		$.label ? Label($, { height:UNIT, style:theme.styles.textNameLeft, string:$.label }) : null,
+		Container($, {
+			left:0, right:0, height:UNIT, active:true, Behavior: REDSliderBehavior,
+			contents: [
+				Content($, { left:0, right:0, top:0, bottom:0, skin:theme.skins.sliderBar }),
+				Content($, { left:0, width:0, top:0, bottom:0, skin:theme.skins.sliderBar, state:1 }),
+				Content($, { left:0, width:UNIT, top:0, bottom:0, skin:theme.skins.sliderThumb  }),
+			],
+		}),
+	],
+}));
+
 let REDSpacer = Content.template($ => ({
 	left:$.left, width:$.width, top:$.top, height:$.height
 }));
 
-class REDTextBehavior extends Behavior {
+
+class REDSwitchBehavior extends Behavior {
+	changeOffset(container, offset) {
+		var bar = container.first;
+		var button = bar.next;
+		if (offset < 0)
+			offset = 0;
+		else if (offset > this.size)
+			offset = this.size;
+		else
+			offset = Math.round(offset);
+		this.offset = offset;
+		bar.state = container.active ? 1 + (this.offset / this.size) : 0;
+		button.state = container.active ? 1 : 0;
+		button.x = bar.x + this.offset + 1;
+	}
 	onCreate(container, data) {
+		this.data = data;
 		data.container = container;
 	}
-	onMessage(container, string) {
-		container.first.last.string = string;
+	onDisplaying(container) {
+		var bar = container.first;
+		var button = bar.next;
+		this.size = bar.width - button.width;
+		this.onUpdate(container);
+	}
+	onTimeChanged(container) {
+		this.changeOffset(container, this.anchor + Math.round(this.delta * container.fraction));
+	}
+	onTouchBegan(container, id, x, y, ticks) {
+		if (container.running) {
+			container.stop();
+			container.time = container.duration;
+		}
+		this.anchor = x;
+		this.moved = false;
+		this.delta = this.offset;
+		container.captureTouch(id, x, y, ticks);
+	}
+	onTouchEnded(container, id, x, y, ticks) {
+		var offset = this.offset;
+		var size =  this.size;
+		var delta = size >> 1;
+		if (this.moved) {
+			if (offset < delta)
+				delta = 0 - offset;
+			else 
+				delta = size - offset;
+		}
+		else {
+			if (offset == 0)
+				delta = size;
+			else if (offset == size)
+				delta = 0 - size;
+			else if (x > (container.x + (container.width >> 1)))
+				delta = size - offset;
+			else
+				delta = 0 - offset;
+		}
+		if (delta) {
+			this.anchor = offset;
+			this.delta = delta;
+			container.duration = 125 * Math.abs(delta) / size;
+			container.time = 0;
+			container.start();
+		}
+		var selection = ((this.offset + delta) == 0) ? 0 : 1;
+		let data = this.data;
+		if (data.selection != selection) {
+			data.selection = selection;
+			data.onChanged();
+		}
+	}
+	onTouchMoved(container, id, x, y, ticks) {
+		this.moved = Math.abs(x - this.anchor) >= 8;
+		this.changeOffset(container, this.delta + x - this.anchor);
+	}
+	onUpdate(container) {
+		this.changeOffset(container, this.data.selection ? this.size : 0);
+	}
+};
+
+let REDSwitch = Row.template($ => ({
+	left:$.left, width:$.width, top:$.top, height:$.height, clip:true,
+	contents: [
+		Label($, { left:0, right:0, top:0, bottom:0, style:theme.styles.textNameLeft, string:$.label }),
+		Container($, {
+			width:60, height:UNIT, active:true, Behavior:REDSwitchBehavior,
+			contents: [
+				Content($, { left:0, right:0, top:0, bottom:0, skin:theme.skins.switchBar }),
+				Content($, { left:0, width:UNIT, top:0, bottom:0, skin:theme.skins.sliderThumb  }),
+			],
+		}),
+	],
+}));
+
+class REDTextBehavior extends Behavior {
+	onCreate(container, data) {
+		this.data = data;
+		data.container = container;
+	}
+	onDisplaying(container) {
+		this.onUpdate(container);
+	}
+	onUpdate(container) {
+		container.first.last.string = this.data.value;
 	}
 }
 
@@ -462,7 +777,11 @@ export {
 	REDApplication,
 	REDButton,
 	REDDropDown,
+	REDGauge,
+	REDNumeric,
+	REDSlider,
 	REDSpacer,
+	REDSwitch,
 	REDTextRowLeft,
 	REDTextRowCenter,
 	REDTextRowRight,

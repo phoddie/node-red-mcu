3db348511bcd5166af5c6be73dd59850d0348541
Patrick Soquet <ps@moddable.tech>
2022-09-29 16:32:21 +0200

    switch color

--- nodes/ui/ui_nodes.js
+++ nodes/ui/ui_nodes.js
@@ -29,6 +29,18 @@ const model = {
 	tabs:[],
 };
 
+function checkValue(value, type) {
+	if (type == "bool") {
+		if (value == "false")
+			value = false;
+		else if (value == "true")
+			value = true;
+		else
+			value = Boolean(value);
+	}
+	return value;
+}
+
 function insert(items, item) {
 	const length = items.length;
 	for (let index = 0; index < length; index++) {
@@ -153,7 +165,7 @@ class UIButtonNode extends UIControlNode {
 		this.color = config.color;
 		this.label = config.label;
 		this.msg = {
-			payload: config.payload,
+			payload: checkValue(config.payload, config.payloadType),
 			topic: config.topic,
 		}
 		this.passthru = config.passthru;
@@ -164,6 +176,170 @@ class UIButtonNode extends UIControlNode {
 	}
 }
 registerConstructor("ui_button", UIButtonNode);
+
+class UIColorPickerNode extends UIControlNode {
+	constructor(id, flow, name) {
+		super(id, flow, name);
+	}
+	onChanged() {
+		const { r, g, b, a } = this.color;
+		let payload;
+		function toHex(n) {
+			return (n < 16) ? "0" + n.toString(16) : n.toString(16);
+		}
+		if (this.format == "hex") {
+			payload = toHex(r) + toHex(g) + toHex(b);
+		}
+		else if (this.format == "hex8") {
+			payload = toHex(r) + toHex(g) + toHex(b) + toHex(a);
+		}
+		else if (this.outformat == "object") {
+			if (this.format == "hsl")
+				payload = this.hsl;
+			else if (this.format == "hsv")
+				payload = this.hsv;
+			else
+				payload = this.rgb;
+		}
+		else {
+			if (this.format == "hsl") {
+				const hsl = this.hsl;
+				payload = `hsl(${hsl.h},${hsl.s},${hsl.l})`;
+			}
+			else if (this.format == "hsv") {
+				const hsv = this.hsv;
+				payload = `hsv(${hsv.h},${hsv.s},${hsv.v})`;
+			}
+			else {
+				const rgb = this.rgb;
+				payload = `rgb(${rgb.r},${rgb.g},${rgb.b})`;
+			}
+		}
+		this.msg.payload = payload;
+		this.send(this.msg);
+	}
+	onMessage(msg) {
+		const payload = msg.payload;
+		const r = 0, g = 0, b = 0, a = 255;
+		if (typeof(payload) == "object") {
+			if (payload.hasOwnProperty("r") && payload.hasOwnProperty("g") && payload.hasOwnProperty("b"))
+				this.rgb = payload;
+			else if (payload.hasOwnProperty("h") && payload.hasOwnProperty("s")) {
+				if (payload.hasOwnProperty("l"))
+					this.hsl = payload;
+				else if (payload.hasOwnProperty("v"))
+					this.hsv = payload;
+			}
+		}
+		else if (typeof(payload) == "string") {
+			debugger
+		}
+		this.container?.delegate("onUpdate");
+		if (this.passthru && (this.msg._msgid != msg._msgid))
+			this.send(this.msg);
+	}
+	onStart(config) {
+		super.onStart(config);
+		this.dynOutput = config.dynOutput;
+		this.format = config.format;
+		this.label = config.label;
+		this.outformat = config.outformat;
+		this.msg = { topic: config.topic }
+		this.passthru = config.passthru;
+		this.color = { r:255, g:255, b:255, a:255 };
+		this.Template = this.lookupTemplate(config, REDSpacer);
+	}
+	onTap() {
+		this.send(this.msg);
+	}
+	get hsl() {
+		const color = this.color;
+		const r = color.r / 255;
+		const g = color.g / 255;
+		const b = color.b / 255;
+		const a = color.a / 255;
+		const l = Math.max(r, g, b);
+		const s = l - Math.min(r, g, b);
+		const h = s ? l === r ? (g - b) / s : l === g ? 2 + (b - r) / s : 4 + (r - g) / s : 0;
+		return {
+			h:60 * h < 0 ? 60 * h + 360 : 60 * h,
+			s:(s ? (l <= 0.5 ? s / (2 * l - s) : s / (2 - (2 * l - s))) : 0),
+			l:((2 * l - s)) / 2,
+			a
+		};
+	}
+	set hsl(it) {
+		let h = Math.mod(it.h, 360);
+		if (h < 0)
+			h += 360;
+		let s = Math.max(0, Math.min(1, it.s));
+		let l = Math.max(0, Math.min(1, it.l));
+		const k = n => (n + h / 30) % 12;
+		const a = s * Math.min(l, 1 - l);
+		const f = n => l - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
+		const color = this.color;
+		color.r = Math.round(255 * f(0));
+		color.g = Math.round(255 * f(8));
+		color.b = Math.round(255 * f(4));
+		if (it.hasOwnProperty("a"))
+			color.a = Math.max(0, Math.min(255, Math.round(it.a * 255)));
+		else
+			color.a = 255;
+	}
+	get hsv() {
+		const color = this.color;
+		const r = color.r / 255;
+		const g = color.g / 255;
+		const b = color.b / 255;
+		const a = color.a / 255;
+		const v = Math.max(r, g, b), n = v - Math.min(r, g, b);
+		const h = n === 0 ? 0 : n && v === r ? (g - b) / n : v === g ? 2 + (b - r) / n : 4 + (r - g) / n;
+		return {
+			h:60 * (h < 0 ? h + 6 : h),
+			s:v && (n / v),
+			v,
+			a
+		};
+	}
+	set hsv(it) {
+		let h = Math.mod(it.h, 360);
+		if (h < 0)
+			h += 360;
+		let s = Math.max(0, Math.min(1, it.s));
+		let v = Math.max(0, Math.min(1, it.v));
+		const k = (n) => (n + h / 60) % 6;
+		const f = (n) => v * (1 - s * Math.max(0, Math.min(k(n), 4 - k(n), 1)));
+		const color = this.color;
+		color.r = Math.round(255 * f(5));
+		color.g = Math.round(255 * f(3));
+		color.b = Math.round(255 * f(1));
+		if (it.hasOwnProperty("a"))
+			color.a = Math.max(0, Math.min(255, Math.round(it.a * 255)));
+		else
+			color.a = 255;
+	}
+	get rgb() {
+		const color = this.color;
+		return {
+			r: color.r,
+			g: color.g,
+			b: color.b,
+			a: color.a / 255
+		}
+	}
+	set rgb(it) {
+		const color = this.color;
+		color.r = Math.max(0, Math.min(255, Math.round(it.r)));
+		color.g = Math.max(0, Math.min(255, Math.round(it.g)));
+		color.b = Math.max(0, Math.min(255, Math.round(it.b)));
+		if (it.hasOwnProperty("a"))
+			color.a = Math.max(0, Math.min(255, Math.round(it.a * 255)));
+		else
+			color.a = 255;
+	}
+}
+registerConstructor("ui_colour_picker", UIColorPickerNode);
+
 
 class UIDropDownNode extends UIControlNode {
 	constructor(id, flow, name) {
@@ -308,7 +484,7 @@ class UISwitchNode extends UIControlNode {
 		this.send(this.msg);
 	}
 	onMessage(msg) {
-		this.selection = this.options.findIndex(option => option.value == msg.payload);
+		this.selection = this.options.findIndex(option => option.value === msg.payload);
 		if (this.selection < 0)
 			this.selection = 0;
 		this.container?.delegate("onUpdate");
@@ -319,8 +495,8 @@ class UISwitchNode extends UIControlNode {
 		super.onStart(config);
 		this.label = config.label;
 		this.options = [
-			{ value:config.offvalue, type:config.offvalueType },
-			{ value:config.onvalue, type:config.onvalueType },
+			{ value:checkValue(config.offvalue, config.offvalueType), type:config.offvalueType },
+			{ value:checkValue(config.onvalue, config.onvalueType), type:config.onvalueType },
 		];
 		this.passthru = config.passthru;
 		this.selection = 0;
--- switch/templates.js
+++ switch/templates.js
@@ -3,6 +3,7 @@ import {
 	registerTemplate
 } from "ui_nodes";
 import {
+	REDBehavior,
 	REDSliderBehavior,
 	REDSwitchBehavior,
 } from "ui_templates";
@@ -17,6 +18,8 @@ const theme = Object.freeze({
 	skins: {
 		brightnessBar: { texture:{ path:"brightness-bar.png" }, color:[LIGHT_GRAY, WHITE], x:0, y:0, width:64, height:60, left:16, right:16 },
 		brightnessIcon: { texture:{ path:"brightness-icon.png" }, color: [WHITE, BLACK], x:0, y:0, width:40, height:40 },
+		colorWheel: { texture:{ path:"color-wheel.png" }, x:0, y:0, width:160, height:160 },
+		colorThumb: { texture:{ path:"color-thumb.png" }, color:["#333333", WHITE], x:0, y:0, width:40, height:40, variants:40 },
 		powerBar: { texture:{ path:"power-bar.png" }, color:[TRANSPARENT, LIGHT_GRAY, GREEN], x:0, y:0, width:120, height:60 },
 		powerThumb: { texture:{ path:"power-thumb.png" }, color:WHITE, x:0, y:0, width:60, height:60 },
 	}
@@ -43,6 +46,88 @@ let BrightnessSlider = Container.template($ => ({
 	],
 }));
 registerTemplate("piu-slider-brightness", BrightnessSlider);
+
+class ColorWheelBehavior extends REDBehavior {
+	onTouchBegan(container, id, x, y, ticks) {
+		container.captureTouch(id, x, y, ticks);
+		this.onTouchMoved(container, id, x, y, ticks);
+	}
+	onTouchMoved(container, id, x, y, ticks) {
+		const data = this.data;
+		const thumb = container.last;
+		const thumbRadius = thumb.width >> 1;
+		const wheelRadius = container.width >> 1;
+		const r = wheelRadius - thumbRadius;
+		const cx = container.x + wheelRadius;
+		const cy = container.y + wheelRadius;
+		x -= cx;
+		y -= cy;
+		const a = Math.atan2(y, x);
+		let d = Math.sqrt(x ** 2 + y ** 2);
+		if (d > r) {
+			x = Math.cos(a) * r;
+			y = Math.sin(a) * r;
+			d = r;
+		}
+		thumb.x = cx + x - thumbRadius;
+		thumb.y = cy + y - thumbRadius;
+		data.hsv = { h:-a * 180 / Math.PI, s:d / r, v:1 };
+		if (data.dynOutput)
+			data.onChanged();
+		thumb.first.invalidate();
+	}
+	onTouchEnded(container, id, x, y, ticks) {
+		this.data.onChanged();
+	}
+	onUpdate(container) {
+		const thumb = container.last;
+		const thumbRadius = thumb.width >> 1;
+		const wheelRadius = container.width >> 1;
+		const r = wheelRadius - thumbRadius;
+		const cx = container.x + wheelRadius;
+		const cy = container.y + wheelRadius;
+		const hsv = this.data.hsv;
+		const a = -hsv.h * Math.PI / 180;
+		const d = hsv.s * r;
+		const x = Math.cos(a) * d;
+		const y = Math.sin(a) * d;
+		thumb.x = cx + x - thumbRadius;
+		thumb.y = cy + y - thumbRadius;
+		thumb.first.invalidate();
+	}
+}
+
+const ColorWheel = Container.template($ => ({
+	left:$.left, width:$.width, top:$.top, height:$.height, clip:true,
+	contents: [
+		Container($, {
+			width:160, height:160, active:true, Behavior:ColorWheelBehavior,
+			contents: [
+				Content($, { skin:theme.skins.colorWheel }),
+				Container($, {
+					left:60, width:40, top:60, height:40,
+					contents: [
+						Port($, {
+							width:40, height:40,
+							Behavior: class extends Behavior {
+								onCreate(port, data) {
+									this.data = data;
+									this.texture = new Texture(theme.skins.colorThumb.texture);
+								}
+								onDraw(port) {
+									const color = this.data.color;
+									port.drawTexture(this.texture, rgb(color.r, color.g, color.b), 0, 0, 0, 0, 40, 40);
+								}
+							},
+						}),
+						Content($, { skin:theme.skins.colorThumb, variant:1 }),
+					],
+				}),
+			],
+		}),
+	],
+}));
+registerTemplate("piu-color-wheel", ColorWheel);
 
 const PowerSwitch = Container.template($ => ({
 	left:$.left, width:$.width, top:$.top, height:$.height, clip:true,

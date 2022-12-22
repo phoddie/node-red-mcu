3978cd27d8e3b8546cf055e075e229a71b0cbd15
Peter Hoddie <peter@moddable.tech>
2022-12-22 13:53:04 -0800

    store colors as numbers in map instead of strings in object properties; add options to set color name and color value arrays (more efficient than message-per-pixel)

--- nodes/rpi-neopixels/rpi-neopixels.js
+++ nodes/rpi-neopixels/rpi-neopixels.js
@@ -23,6 +23,12 @@ import Timer from "timer";
 import NeoPixel from "neopixel";
 import colors from "colors";
 
+const Timing_WS2812B = Object.freeze({
+    mark:  { level0: 1, duration0: 900, level1: 0, duration1: 350 },
+    space: { level0: 1, duration0: 350, level1: 0, duration1: 900 },
+    reset: { level0: 0, duration0: 100, level1: 0, duration1: 100 }
+}, true);;
+
 class NeopixelsNode extends Node {
 	#np;
 	#fgnd;
@@ -63,11 +69,14 @@ class NeopixelsNode extends Node {
 		if (globalThis.lights)		// for compatibility with hosts that create the NeoPixel driver (M5Atom-Matrix) 
 			this.#np = globalThis.lights;
 		else
-			this.#np = new NeoPixel({length: config.pixels, pin: parseInt(config.gpio), order: config.rgb.toUpperCase()});
+			this.#np = new NeoPixel({length: config.pixels, pin: 10 /* parseInt(config.gpio) */, order: config.rgb.toUpperCase(), timing: Timing_WS2812B});
 
-		this.#bgnd = this.#np.makeRGB(...this.#bgnd.split(","));
-		this.#fgnd = this.#np.makeRGB(...this.#fgnd.split(","));
+		this.#bgnd = this.#np.makeRGB(this.#bgnd.r, this.#bgnd.g, this.#bgnd.b);
+		this.#fgnd = this.#np.makeRGB(this.#fgnd.r, this.#fgnd.g, this.#fgnd.b);
 		this.#needle = this.#np.makeRGB(255, 255, 255); 
+
+		if (undefined !== config.brightness)
+			this.#np.brightness = parseFloat(config.brightness) / 100 * 255; 
 
 		if (!this.#mode.startsWith("shift")) {
 			this.#np.fill(this.#bgnd);
@@ -87,141 +96,165 @@ class NeopixelsNode extends Node {
 				this.#np.brightness = brightness;
 		}
 
-		if (undefined === msg.payload)
+		if ((undefined === msg.payload) && (null == msg.colorNames) && (null === msg.colors))
 			return void done();
 
-		let payload = msg.payload.toString();
-		let parts = payload.split(",");
-		switch (parts.length) {
-			case 1:
-				if (isNaN(payload)) { // it's a single colour word so set background
-					const color = colors.getRGB(payload);
+		if (msg.colorNames) {
+			const colorNames = msg.colorNames;
+			let i = msg.start ?? 0;
+			let length = Math.min(this.#np.length, i + colorNames.length);
+			for (; i < length; i++) {
+				let c = colorNames[i];
+				if (c) {
+					c = colors.getRGB(c);
+					this.setPixel(i, this.#np.makeRGB(c.r, c.g, c.b));
+				}
+			}
+		}
+		else if (msg.colors) {
+			const colors = msg.colors;
+			let i = msg.start ?? 0;
+			let length = Math.min(this.#np.length, i + colors.length);
+			for (; i < length; i++) {
+				let c = colors[i];
+				if (undefined !== c)
+					this.setPixel(i, this.#np.makeRGB(c >> 16, c >> 8, c));
+			}
+		}
+		else {
+			const payload = msg.payload;
+			let parts = payload.toString().split(",");
+			switch (parts.length) {
+				case 1:
+					if (isNaN(payload)) { // it's a single colour word so set background
+						const color = colors.getRGB(payload);
+						if (!color) {
+							this.warn("Invalid payload: " + payload);
+							return void done();
+						}
+
+						this.#bgnd = this.#np.makeRGB(color.r, color.g, color.b);
+						this.fill(this.#bgnd);
+					}
+					else { // it's a single number so just draw bar
+						let ll = Number(payload);
+						if (ll < 0)
+							ll = 0;
+						if (this.#mode.indexOf("pcent") >= 0) {
+							ll = Math.round(ll / 100 * this.#np.length + 0.5);
+							ll = Math.min(ll, 100);
+						}
+						else
+							ll = Math.min(ll, this.#np.length - 1);
+						if (this.#mode.indexOf("need") >= 0) {
+							this.fill(this.#bgnd, 0, ll);
+							this.setPixel(ll, this.#needle);
+							this.fill(this.#bgnd, ll + 1, this.#np.length);
+						}
+						else {
+							ll = ll - 1;
+							this.fill(this.#fgnd, 0, ll);
+							this.fill(this.#bgnd, ll + 1, this.#np.length);
+						}
+					}
+					break;
+
+				case 2: {	 // it's a colour and length
+					if (isNaN(parseInt(parts[1])))
+						parts = parts.reverse();
+						
+					let color = colors.getRGB(parts[0]); 				
 					if (!color) {
-						this.warn("Invalid payload: " + payload);
+						this.warn("Invalid color: " + payload);
 						return void done();
 					}
+					color = this.#np.makeRGB(color.r, color.g, color.b);
 
-					this.#bgnd = this.#np.makeRGB(...color.split(","));
-					this.fill(this.#bgnd);
-				}
-				else { // it's a single number so just draw bar
-					let ll = Number(payload);
-					if (ll < 0)
-						ll = 0;
-					if (this.#mode.indexOf("pcent") >= 0) {
-						ll = Math.round(ll / 100 * this.#np.length + 0.5);
-						ll = Math.min(ll, 100);
-					}
-					else
-						ll = Math.min(ll, this.#np.length - 1);
+					let l = Number(parts[1]);
+					if (this.#mode.indexOf("pcent") >= 0)
+						l = Math.round(l / 100 * this.#np.length + 0.5);
 					if (this.#mode.indexOf("need") >= 0) {
-						this.fill(this.#bgnd, 0, ll);
-						this.setPixel(ll, this.#needle);
-						this.fill(this.#bgnd, ll + 1, this.#np.length);
+						this.#needle = color;
+
+						this.fill(this.#bgnd, 0, l);
+						this.setPixel(l, this.#needle);
+						this.fill(this.#bgnd, l + 1, this.#np.length);
 					}
 					else {
-						ll = ll - 1;
-						this.fill(this.#fgnd, 0, ll);
-						this.fill(this.#bgnd, ll + 1, this.#np.length);
+						l = l - 1;
+						this.#fgnd = color;
+
+						this.fill(this.#fgnd, 0, l);
+						this.fill(this.#bgnd, l + 1, this.#np.length);
 					}
-				}
-				break;
+					} break;
 
-			case 2: {	 // it's a colour and length
-				if (isNaN(parseInt(parts[1])))
-					parts = parts.reverse();
-					
-				let color = colors.getRGB(parts[0]); 				
-				if (!color) {
-					this.warn("Invalid color: " + payload);
-					return void done();
-				}
-				color = this.#np.makeRGB(...color.split(","));
+				case 3: {		// RGB triple -- either shift or fill
+					const color = this.#np.makeRGB(parts[0], parts[1], parts[2]);
+					this.#bgnd = color;
 
-				let l = Number(parts[1]);
-				if (this.#mode.indexOf("pcent") >= 0)
-					l = Math.round(l / 100 * this.#np.length + 0.5);
-				if (this.#mode.indexOf("need") >= 0) {
-					this.#needle = color;
+					if ("shiftu" === this.#mode) {
+						if (!this.#wipe) { 
+							const pixels = new Uint8Array(this.#np);
+							pixels.copyWithin(3, 0, 3 * (pixels.length - 1));
+							this.setPixel(0, color);
+							this.#np.update();
+							return void done();
+						}
 
-					this.fill(this.#bgnd, 0, l);
-					this.setPixel(l, this.#needle);
-					this.fill(this.#bgnd, l + 1, this.#np.length);
-				}
-				else {
-					l = l - 1;
-					this.#fgnd = color;
+						let i = 0, c = color;
+						this.#wipe.wipers.push(() => {
+							const prev = c;
+							c = this.#np.getPixel(i);
 
-					this.fill(this.#fgnd, 0, l);
-					this.fill(this.#bgnd, l + 1, this.#np.length);
-				}
-				} break;
+							this.#np.setPixel(i++, prev);
+							this.#np.update();
+							return i >= this.#np.length;
+						});
+						if (1 === this.#wipe.wipers.length)
+							Timer.schedule(this.#wipe, 0, this.#wipe.interval);
+					}
+					else if ("shiftd" === this.#mode) {
+						if (!this.#wipe) { 
+							const pixels = new Uint8Array(this.#np);
+							pixels.copyWithin(0, 3, 3 * (pixels.length - 1));
+							this.setPixel(this.#np.length - 1, color);
+							this.#np.update();
+							return void done();
+						}
 
-			case 3: {		// RGB triple -- either shift or fill
-				const color = this.#np.makeRGB(parts[0], parts[1], parts[2]);
-				this.#bgnd = color;
+						let i = this.#np.length - 1, c = color;
+						this.#wipe.wipers.push(() => {
+							const prev = c;
+							c = this.#np.getPixel(i);
 
-				if ("shiftu" === this.#mode) {
-					if (!this.#wipe) { 
-						const pixels = new Uint8Array(this.#np);
-						pixels.copyWithin(3, 0, 3 * (pixels.length - 1));
-						this.setPixel(0, color);
-						this.#np.update();
+							this.#np.setPixel(i--, prev);
+							this.#np.update();
+							return i < 0;
+						});
+						if (1 === this.#wipe.wipers.length)
+							Timer.schedule(this.#wipe, 0, this.#wipe.interval);
+					}
+					else
+						this.fill(color);
+					} break;
+
+				case 4:	// set nth pixel: n, r, g, b
+					if (!this.#mode.startsWith("p"))
 						return void done();
-					}
+					this.setPixel(parts[0], this.#np.makeRGB(parts[1], parts[2], parts[3]));
+					break;
 
-					let i = 0, c = color;
-					this.#wipe.wipers.push(() => {
-						const prev = c;
-						c = this.#np.getPixel(i);
-
-						this.#np.setPixel(i++, prev);
-						this.#np.update();
-						return i >= this.#np.length;
-					});
-					if (1 === this.#wipe.wipers.length)
-						Timer.schedule(this.#wipe, 0, this.#wipe.interval);
-				}
-				else if ("shiftd" === this.#mode) {
-					if (!this.#wipe) { 
-						const pixels = new Uint8Array(this.#np);
-						pixels.copyWithin(0, 3, 3 * (pixels.length - 1));
-						this.setPixel(this.#np.length - 1, color);
-						this.#np.update();
+				case 5: {		// set pixels x through y: x, y, r, g, b
+					if (!this.#mode.startsWith("p"))
 						return void done();
-					}
-
-					let i = this.#np.length - 1, c = color;
-					this.#wipe.wipers.push(() => {
-						const prev = c;
-						c = this.#np.getPixel(i);
-
-						this.#np.setPixel(i--, prev);
-						this.#np.update();
-						return i < 0;
-					});
-					if (1 === this.#wipe.wipers.length)
-						Timer.schedule(this.#wipe, 0, this.#wipe.interval);
-				}
-				else
-					this.fill(color);
-				} break;
-
-			case 4:	// set nth pixel: n, r, g, b
-				if (!this.#mode.startsWith("p"))
+					const x = parseInt(parts[0]), y = parseInt(parts[1]);
+					this.fill(this.#np.makeRGB(parts[2], parts[3], parts[4]), x, y - x);
+					} break;
+				
+				default:
 					return void done();
-				this.setPixel(parts[0], this.#np.makeRGB(parts[1], parts[2], parts[3]));
-				break;
-
-			case 5: {		// set pixels x through y: x, y, r, g, b
-				if (!this.#mode.startsWith("p"))
-					return void done();
-				const x = parseInt(parts[0]), y = parseInt(parts[1]);
-				this.fill(this.#np.makeRGB(parts[2], parts[3], parts[4]), x, y - x);
-				} break;
-			
-			default:
-				return void done();
+			}
 		}
 
 		if (this.#wipe) {

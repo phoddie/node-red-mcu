f661f0069b7f4026a754d76b1aaac283ccb73721
Peter Hoddie <peter@moddable.tech>
2023-01-02 21:17:05 -0800

    oops - remove debug code

--- main.js
+++ main.js
@@ -2,8 +2,3 @@ import "nodered";	// import for global side effects
 import flows from "flows";
 
 RED.build(flows);
-
-import Timer from "timer";
-import WiFi from "wifi";
-
-Timer.set(() => WiFi.disconnect(), 5_000);

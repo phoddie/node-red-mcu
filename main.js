1ce679555fb8c60391d99901c76224d6e60b0435
Peter Hoddie <peter@moddable.tech>
2023-01-02 21:08:07 -0800

    simpler integration of Wi-Fi reconnect (inspired by comment from @ralphwetzel)

--- main.js
+++ main.js
@@ -1,55 +1,9 @@
 import "nodered";	// import for global side effects
 import flows from "flows";
 
-import config from "mc/config";
-import Time from "time";
-import WiFi from "wifi/connection";
-import Net from "net";
-import SNTP from "sntp";
+RED.build(flows);
 
-WiFi.mode = 1;
-if (config.ssid) {
-	let once = true;
+import Timer from "timer";
+import WiFi from "wifi";
 
-	new WiFi({ssid: config.ssid, password: config.password}, function(msg, code) {
-	   switch (msg) {
-		   case WiFi.gotIP:
-				trace(`IP address ${Net.get("IP")}\n`);
-
-				if (!config.sntp) {
-					if (once) {
-						once = false;
-						RED.build(flows);
-					}
-					return;
-				}
-
-				new SNTP({host: config.sntp}, function(message, value) {
-					if (1 === message) {
-						trace("got time\n");
-						Time.set(value);
-
-						if (once) {
-							once = false;
-							RED.build(flows);
-						}
-					}
-					else if (message < 0)
-						trace("can't get time\n");
-					else
-						return;
-				});
-				break;
-
-			case WiFi.connected:
-				trace(`Wi-Fi connected to "${Net.get("SSID")}"\n`);
-				break;
-
-			case WiFi.disconnected:
-				trace((-1 === code) ? "Wi-Fi password rejected\n" : "Wi-Fi disconnected\n");
-				break;
-		}
-	});
-}
-else
-	RED.build(flows);
+Timer.set(() => WiFi.disconnect(), 5_000);

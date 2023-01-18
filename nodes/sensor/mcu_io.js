7e3b767c9742938a9fde862b02bdb9a13305ef1a
Patrick Soquet <ps@moddable.tech>
2023-01-19 00:35:47 +0100

    Analog, Pulse Count, PWM, Clock editors

--- nodes/sensor/mcu_io.js
+++ nodes/sensor/mcu_io.js
@@ -1,12 +1,27 @@
 module.exports = function(RED) {
     function DigitalInNode(config) {
-        RED.nodes.createNode(this,config);
+        RED.nodes.createNode(this, config);
 		console.log(config)
     }
     RED.nodes.registerType("digital_in", DigitalInNode);
     function DigitalOutNode(config) {
-        RED.nodes.createNode(this,config);
+        RED.nodes.createNode(this, config);
 		console.log(config)
     }
     RED.nodes.registerType("digital_out", DigitalOutNode);
+    function AnalogNode(config) {
+        RED.nodes.createNode(this, config);
+		console.log(config)
+    }
+    RED.nodes.registerType("analog", AnalogNode);
+    function PulseCountNode(config) {
+        RED.nodes.createNode(this, config);
+		console.log(config)
+    }
+    RED.nodes.registerType("pulse_count", PulseCountNode);
+    function PWMNode(config) {
+        RED.nodes.createNode(this, config);
+		console.log(config)
+    }
+    RED.nodes.registerType("pwm", PWMNode);
 }

8224a11d72ff06ce710bd0201a16457a384e7b59
Peter Hoddie <peter@moddable.tech>
2023-02-13 21:13:43 -0800

    main supports UI nodes too (same as mod's host)

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
 import "nodered";	// import for global side effects
-import flows from "flows";
+import Modules from "modules";
 
-RED.build(flows);
+if (!Modules.has("flows"))
+	trace("no flows installed\n");
+else {
+
+	const flows = Modules.importNow("flows");
+	RED.build(flows);
+
+	if (globalThis.REDTheme) {
+		if (!Modules.has("ui_nodes") || !Modules.has("ui_templates"))
+			trace("flow neeeds UI nodes; not build into host \n");
+		else {
+			const buildModel = Modules.importNow("ui_nodes");
+			const templates = Modules.importNow("ui_templates");
+			const REDApplication = templates.REDApplication; 
+			if (REDApplication) {
+				try {
+					const model = buildModel();
+					new REDApplication(model, { commandListLength:4096, displayListLength:+4096, touchCount:1, pixels: 240 * 48 });
+				}
+				catch {
+				}
+			}
+		}
+	} 
+}
